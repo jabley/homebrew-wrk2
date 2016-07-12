c2ed15c62dc71eb1c2890ac8925872f7ce5b90db
James Abley <james.abley@gmail.com>
2016-07-12 12:45:41 +0100

    Remove wrk download URL

--- wrk2.rb
+++ wrk2.rb
@@ -1,7 +1,6 @@
 class Wrk2 < Formula
   desc "HTTP benchmarking tool"
   homepage "https://github.com/giltene/wrk2"
-  url "https://github.com/wg/wrk/archive/4.0.2.tar.gz"
   sha256 "c4250acb6921c13f8dccfc162d894bd7135a2979"
   head "https://github.com/giltene/wrk2.git"
 
